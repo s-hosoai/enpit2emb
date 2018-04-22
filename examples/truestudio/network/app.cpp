35e7cbe164680a4d0dcf49421cc622aee8251933
s-hosoai <shintaro.hosoai@change-vision.com>
2018-04-22 12:36:53 +0900

    ライントレースデータの取得

--- examples/truestudio/network/app.cpp
+++ examples/truestudio/network/app.cpp
@@ -2,18 +2,18 @@
 #include "kernel_cfg.h"
 #include "app.h"
 
+#include "Zumo.h"
 #include "mbed.h"
-
 #include "app_config.h"
-
 #include "GR_PEACH_WlanBP3595.h"
 
+#define LWIP_DBG_ON
 GR_PEACH_WlanBP3595 wlan;
 Serial pc(USBTX, USBRX);
+Zumo zumo;
 
 void task_main(intptr_t exinf) {
 	pc.baud(115200);
-	const char *buf;
 	pc.printf("\r\ninitializing\r\n");
 	int ret = wlan.init(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
 	if(ret!=0){
@@ -29,20 +29,46 @@ void task_main(intptr_t exinf) {
 	}
 
 	TCPSocketConnection socket;
+	socket.set_blocking(true, 100);
 	pc.printf("socket connecting\r\n");
 	ret = socket.connect(SERVER_ADDRESS, SERVER_PORT);
 	if(ret!=0){
 		pc.printf("socket connect error\r\n");
 		return;
 	}
-
-	char* data = strdup("a");
-	ret = socket.send(data, strlen(data));
-	if(ret!=0){
-		pc.printf("socket send error\r\n");
-		return;
+	
+	short x, y, z;
+    unsigned int irValues[6];
+    int error;
+	while(true){
+		char accLog[50];
+		char irLog[50];
+        zumo.getAcceleration(&x, &y, &z);
+        sprintf(accLog, "Acceleration : %d, %d, %d\r\n",x, y, z); 
+        pc.printf("%s", accLog);
+    	ret = socket.send_all(accLog, strlen(accLog));
+    	if(ret<0){
+    		pc.printf("socket send error : %d\r\n", ret);
+    	}
+    	
+        zumo.readAnalogIrValue(irValues);
+        error = irValues[3]-500;
+		if (error > 50) {
+			error = 50;
+		} else if (error < -50) {
+			error = -50;
+		}
+		
+        sprintf(irLog, "IR[3] value : %d\r\n", irValues[3]);
+        pc.printf("%s", irLog);
+    	ret = socket.send_all(irLog, strlen(irLog));
+    	if(ret<0){
+    		pc.printf("socket send error : %d\r\n", ret);
+    	}
+		zumo.driveTank(100 + error, 100 - error);
+		
 	}
-
+	
 	socket.close();
 	wlan.disconnect();
 }
